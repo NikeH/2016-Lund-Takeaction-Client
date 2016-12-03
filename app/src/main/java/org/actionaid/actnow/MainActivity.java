01a8449291cbfaa93e55396d1112dcb86ec3a348
Ahmed <ahmed91abbas@hotmail.com>
2016-12-03 12:34:11 +0100

    fixed layout of upcoming event

--- app/src/main/java/org/actionaid/actnow/MainActivity.java
+++ app/src/main/java/org/actionaid/actnow/MainActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +18,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewGroup linearLayout = (ViewGroup) findViewById(R.id.activity_main);
 
+        final RelativeLayout topRelativeLayout = (RelativeLayout) findViewById(R.id.activity_main);
+
+
+
+            final TextView topicTest = new TextView(this);
+            topicTest.setText("2/12 Demonstration for civil rights");
+            topicTest.setGravity(Gravity.LEFT);
+            topRelativeLayout.addView(topicTest);
+            RelativeLayout.LayoutParams textviewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            textviewParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+            topicTest.setLayoutParams(textviewParams);
+
+            final Button actNow = new Button(this);
+            actNow.setText("Act now!");
+            RelativeLayout.LayoutParams buttonParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            buttonParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+            actNow.setLayoutParams(buttonParams);
+            actNow.setGravity(Gravity.RIGHT);
+            topRelativeLayout.addView(actNow);
     }
 
     public void openSettings(View view){
