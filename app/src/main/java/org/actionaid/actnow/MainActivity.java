2ff5d43adbb76523b74eea18b8a7950a29f479fc
Daniel Bjarnestig <danielbjarnestig@gmail.com>
2016-12-03 18:20:57 +0100

    Add a basic ListView

--- app/src/main/java/org/actionaid/actnow/MainActivity.java
+++ app/src/main/java/org/actionaid/actnow/MainActivity.java
@@ -7,19 +7,51 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Actions");
         setContentView(R.layout.activity_main);
+
+        lv = (ListView) findViewById(R.id.articleList);
+
+        ArrayList<String> articles = new ArrayList<String>();
+        articles.add("asdf");
+        articles.add("ghjk");
+        articles.add("ghjk");
+        articles.add("ghjk");
+        articles.add("ghjk");
+        articles.add("ghjk");
+        articles.add("ghjk");
+        articles.add("ghjk");
+        articles.add("ghjk");
+        articles.add("ghjk");
+        articles.add("ghjk");
+        articles.add("ghjk");
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                articles );
+
+        lv.setAdapter(arrayAdapter);
+    }
 
 
 /*        final RelativeLayout topRelativeLayout = (RelativeLayout) findViewById(R.id.activity_main);
