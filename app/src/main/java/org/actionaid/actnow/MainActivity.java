70a62f22cff91639d7b4e52e2a657f04d35aea7c
Ahmed <ahmed91abbas@hotmail.com>
2016-12-03 11:00:08 +0100

    added settings activity

--- app/src/main/java/org/actionaid/actnow/MainActivity.java
+++ app/src/main/java/org/actionaid/actnow/MainActivity.java
@@ -1,7 +1,12 @@
 package org.actionaid.actnow;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewGroup linearLayout = (ViewGroup) findViewById(R.id.activity_main);
+
+    }
+
+    public void openSettings(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
