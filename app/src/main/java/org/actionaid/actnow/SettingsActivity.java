8c391e6e9a6574feae917c99ce43c2b80632c9b3
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-12-03 15:53:35 +0100

    Make the save button send you back without history

--- app/src/main/java/org/actionaid/actnow/SettingsActivity.java
+++ app/src/main/java/org/actionaid/actnow/SettingsActivity.java
@@ -1,7 +1,11 @@
 package org.actionaid.actnow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void clickSave(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        // Make the back button not go back to the settings menu once you're out of here
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
