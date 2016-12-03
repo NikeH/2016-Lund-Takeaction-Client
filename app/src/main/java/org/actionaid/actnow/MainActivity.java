aa20a93e191a9507705a8a11e4e7ddae90144c9a
Ahmed <ahmed91abbas@hotmail.com>
2016-12-03 21:31:48 +0100

    refactoring

--- app/src/main/java/org/actionaid/actnow/MainActivity.java
+++ app/src/main/java/org/actionaid/actnow/MainActivity.java
@@ -57,38 +57,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-/*        final RelativeLayout topRelativeLayout = (RelativeLayout) findViewById(R.id.activity_main);
-
-        final TextView topicTest = new TextView(this);
-        topicTest.setText("2/12 Demonstration for civil rights");
-        topicTest.setGravity(Gravity.LEFT);
-        topRelativeLayout.addView(topicTest);
-        RelativeLayout.LayoutParams textviewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        textviewParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
-        topicTest.setLayoutParams(textviewParams);
-
-        final Button actNow = new Button(this);
-        actNow.setText("Act now!");
-        RelativeLayout.LayoutParams buttonParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        buttonParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-        actNow.setLayoutParams(buttonParams);
-        actNow.setGravity(Gravity.RIGHT);
-        actNow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), TakeAction.class);
-                startActivity(i);
-            }
-
-        });
-        topRelativeLayout.addView(actNow);*/
-
-
     public void openSettings(View view){
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
-        Log.i("INFO", data);
     }
 
     public void fillData(String data) {
