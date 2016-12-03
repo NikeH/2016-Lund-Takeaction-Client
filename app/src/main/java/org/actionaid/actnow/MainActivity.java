69a4a7c93b621b95d79b348d9029f9b7b33ff1ff
Daniel Bjarnestig <danielbjarnestig@gmail.com>
2016-12-03 17:59:27 +0100

    The button is centered and list is on screen, just not filled with anything

--- app/src/main/java/org/actionaid/actnow/MainActivity.java
+++ app/src/main/java/org/actionaid/actnow/MainActivity.java
@@ -22,31 +22,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+/*        final RelativeLayout topRelativeLayout = (RelativeLayout) findViewById(R.id.activity_main);
 
-        final RelativeLayout topRelativeLayout = (RelativeLayout) findViewById(R.id.activity_main);
+        final TextView topicTest = new TextView(this);
+        topicTest.setText("2/12 Demonstration for civil rights");
+        topicTest.setGravity(Gravity.LEFT);
+        topRelativeLayout.addView(topicTest);
+        RelativeLayout.LayoutParams textviewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        textviewParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+        topicTest.setLayoutParams(textviewParams);
 
-            final TextView topicTest = new TextView(this);
-            topicTest.setText("2/12 Demonstration for civil rights");
-            topicTest.setGravity(Gravity.LEFT);
-            topRelativeLayout.addView(topicTest);
-            RelativeLayout.LayoutParams textviewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-            textviewParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
-            topicTest.setLayoutParams(textviewParams);
-
-            final Button actNow = new Button(this);
-            actNow.setText("Act now!");
-            RelativeLayout.LayoutParams buttonParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-            buttonParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-            actNow.setLayoutParams(buttonParams);
-            actNow.setGravity(Gravity.RIGHT);
-            actNow.setOnClickListener(new View.OnClickListener() {
+        final Button actNow = new Button(this);
+        actNow.setText("Act now!");
+        RelativeLayout.LayoutParams buttonParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        buttonParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+        actNow.setLayoutParams(buttonParams);
+        actNow.setGravity(Gravity.RIGHT);
+        actNow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Intent i = new Intent(getApplicationContext(), TakeAction.class);
+                Intent i = new Intent(getApplicationContext(), TakeAction.class);
                 startActivity(i);
             }
-            });
-        topRelativeLayout.addView(actNow);
+        });
+        topRelativeLayout.addView(actNow);*/
     }
 
     public void openSettings(View view){
