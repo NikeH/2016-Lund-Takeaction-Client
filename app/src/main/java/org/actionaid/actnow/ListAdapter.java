df1b7a4a25fe11ed333f33511b45337df0f8efb7
Ahmed <ahmed91abbas@hotmail.com>
2016-12-04 11:52:29 +0100

    refactoring

--- app/src/main/java/org/actionaid/actnow/ListAdapter.java
+++ app/src/main/java/org/actionaid/actnow/ListAdapter.java
@@ -3,13 +3,11 @@ package org.actionaid.actnow;
 // Slightly inspired by https://stackoverflow.com/a/8166802/3605357
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
